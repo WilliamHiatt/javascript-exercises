ca22547c5edc83b2942ab600d78958a6e9d4d03e
William Hiatt <seahawks24-7@hotmail.com>
2024-10-12 16:37:03 -0600

    Finished the find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,29 @@
-const findTheOldest = function() {
+function getAge(user)
+{
+    if("yearOfDeath" in user)
+    {
+        return user.yearOfDeath - user.yearOfBirth;
+    }
+    else
+    {
+        const d = new Date();
+        let year = d.getFullYear();
+
+        return year - user.yearOfBirth;
+    }
+}
+
+const findTheOldest = function(peopleArr) {
+    let oldestPerson = peopleArr[0];
+
+    peopleArr.forEach(function (user){
+        if(getAge(oldestPerson) < getAge(user))
+        {
+            oldestPerson = user;
+        }
+    });
+
+    return oldestPerson;
 
 };
 
