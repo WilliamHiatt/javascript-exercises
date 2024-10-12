3707ea9c5d0b45f7fde934d41983a566c9d29b27
William Hiatt <seahawks24-7@hotmail.com>
2024-10-12 15:32:15 -0600

    Finished the palindromes section

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,33 @@
-const palindromes = function () {
+const palindromes = function (incomingStr) {
+    let pntr1 = 0;
+    let pntr2 = incomingStr.length - 1;
+
+    while(pntr1 <= pntr2)
+    {
+        if(!(incomingStr[pntr1].match(/^[a-zA-Z0-9]+$/) !== null) || !(incomingStr[pntr2].match(/^[a-zA-Z0-9]+$/) !== null))
+        {
+            if(!(incomingStr[pntr1].match(/^[a-zA-Z0-9]+$/) !== null))
+            {
+                pntr1++;
+            }
+
+            if(!(incomingStr[pntr2].match(/^[a-zA-Z0-9]+$/) !== null))
+                {
+                    pntr2--;
+                }
+        }
+        else if(incomingStr[pntr1].toLowerCase() != incomingStr[pntr2].toLowerCase())
+        {
+            return false;
+        }
+        else
+        {
+            pntr1++;
+            pntr2--;
+        }
+    }
+
+    return true;
 
 };
 
