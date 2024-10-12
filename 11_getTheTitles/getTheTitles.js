911978e97639e9ce83df6cdcf04dc57b498fd966
William Hiatt <seahawks24-7@hotmail.com>
2024-10-12 16:10:08 -0600

    Fnished GetTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(arrOfBooks) {
+    let returnArr = []
 
+    arrOfBooks.forEach((obj) => returnArr.push(obj.title));
+
+    return returnArr;
 };
 
 // Do not edit below this line
