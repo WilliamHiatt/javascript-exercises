8bf1ada6894de51dbb287a884f1547c3bef1c622
William Hiatt <seahawks24-7@hotmail.com>
2024-10-12 09:32:27 -0600

    Finished the calculator JS exercises.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(incomingArr) {
+  let sum = 0;
+	incomingArr.forEach((x) => sum += x);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(incomingArr) {
+  let sum = 1;
+  incomingArr.forEach((x) => sum *= x)
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let sum = 1;
+  if (x == 0)
+  {
+    return 1;
+  }
+	for (x; x > 0; x--)
+  {
+    sum *= x;
+  }
+  return sum;
 };
 
 // Do not edit below this line
