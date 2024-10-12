176fbe7f2b8c3501a65b13e9a2a22f79da50ac31
William Hiatt <seahawks24-7@hotmail.com>
2024-10-12 15:46:40 -0600

    FInished the fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num == 0)
+    {
+        return 0;
+    }
+
+    if (num < 0)
+    {
+        return "OOPS";
+    }
+
+    let arr = [];
+
+    for (let x = 0; x <= num; x++)
+    {
+        if(arr.length > 2)
+        {
+            arr[x] = arr[x-1] + arr[x-2];
+        }
+        else
+        {
+            arr[x] = 1;
+        }
+    }
+
+
+    return arr[arr.length - 1];
 
 };
 
